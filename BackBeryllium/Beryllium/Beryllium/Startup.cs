7f73b029961681c8772c310911a528bfff12e130
fatima-28 <2n6gddb@code.edu.az>
2022-06-15 23:33:29 -0700

    Admin Panel Created

--- BackBeryllium/Beryllium/Beryllium/Startup.cs
+++ BackBeryllium/Beryllium/Beryllium/Startup.cs
@@ -47,6 +47,9 @@ namespace Beryllium
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{Id?}"
 );
